c90032bdcc4cca9f9ead1233b23de57c37940531
sridevithanigasalam <164857693+sridevithanigasalam@users.noreply.github.com>
2025-03-13 10:30:32 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,46 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-
-helpful_links = [
-    "https://docs.streamlit.io",
-    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-]
-
-# Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(" Choose the fruits you want in your custom Smoothie! ")
-
-# option = st.selectbox(
-#     "What is your favorite fruit?",
-#     ("Banana", "Strawberries", "Peaches"))
-
-# st.write("Your favorite fruit is:", option)
-
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(" Order that needto filled. ")
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+
 # st.dataframe(data=my_dataframe, use_container_width=True)
+editable_df = st.data_editor(my_dataframe)
 
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-)
+submitted = st.button('Submit')
 
-if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
+if submitted:
+    og_dataset = session.table("smoothies.public.orders")
+    edited_dataset = session.create_dataframe(editable_df)
     
-    ingredients_string = ''
+    try:
+        og_dataset.merge(edited_dataset
+                 , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                 , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                )
+        st.success("order(s) Updated!",icon ="👍" )
+    except:
+        st.write('Something went wrong.')
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+else:
+    st.success("there are no pending orders right now", icon ="👍")
+# if submitted:
+#     st.success("someone clicked the button,", icon ="👍")
 
-    # st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
-
-    # st.write(my_insert_stmt)
-    time_to_insert = st.button("Submit Order")
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
-    
+# og_dataset = session.table("smoothies.public.orders")
+# edited_dataset = session.create_dataframe(editable_df)
+# og_dataset.merge(edited_dataset
+#                  , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+#                  , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+#                 )
