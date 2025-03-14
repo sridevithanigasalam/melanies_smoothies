d507f71d2ba670db92a20b26fae497ad35bf4c5f
sridevithanigasalam <164857693+sridevithanigasalam@users.noreply.github.com>
2025-03-14 12:16:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
-        fv_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
         # st.subheader(fruit_chosen + ' Nutrition Information')
         # fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
